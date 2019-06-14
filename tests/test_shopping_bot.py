7b020d1e63f2a25080603f72af625464434c571a
Mpho Mphego <mpho112@gmail.com>
2019-06-14 16:46:03 +0200

    Tests...

---
+++ tests/test_shopping_bot.py
@@ -0,0 +1,31 @@
+import unittest
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.common.keys import Keys
+
+
+class MakroBot(unittest.TestCase):
+
+    def setUp(self):
+        self._options = Options()
+        self._options.headless = False
+        self.driver = webdriver.Firefox(options=self._options, timeout=30)
+
+    def test_search(self):
+        driver = self.driver
+        driver.get("https://www.makro.co.za/")
+        self.assertIn("Makro", driver.title)
+        elem = driver.find_element_by_id("js-site-search-input")
+        elem.send_keys("beer")
+        elem.send_keys(Keys.RETURN)
+        assert "No results found." not in driver.page_source
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
